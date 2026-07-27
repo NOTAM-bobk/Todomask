d5d34931e928b263fca4fb3f0e372f3e86add4fe
NOTAM <sawyerbobk563@gmail.com>
2026-07-27 12:25:13 -0500

    Add onboarding flow to main component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Onboarding, { hasOnboarded } from './Onboarding.jsx'
 import './index.css'
 
 if ('serviceWorker' in navigator) {
@@ -9,8 +10,28 @@ if ('serviceWorker' in navigator) {
   })
 }
 
+function Root() {
+  // Checks localStorage to see if they've completed onboarding before the first render
+  const [onboarded, setOnboarded] = useState(hasOnboarded)
+  
+  if (!onboarded) {
+    return (
+      <Onboarding
+        onComplete={() => setOnboarded(true)}
+        onLogin={() => { 
+          // You can add your route to your login screen here
+          console.log("Routing to login...") 
+        }}
+      />
+    )
+  }
+  
+  // If they have onboarded, show the main app
+  return <App />
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 )
